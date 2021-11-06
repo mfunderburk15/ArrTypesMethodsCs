68f70948c6b452620e50ccc969578a2fa7db30f7
masen.funderburk@gmail.com <masen.funderburk@gmail.com>
2021-11-05 21:41:15 -0500

    Length and IndexOf()

--- ArrMethodsCs/Program.cs
+++ ArrMethodsCs/Program.cs
@@ -6,7 +6,14 @@ namespace ArrMethodsCs
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] numbers = new int[] {3, 7, 9, 2, 14, 6 };
+
+            //Length
+            Console.WriteLine("Length: " + numbers.Length);
+
+            //IndexOf()
+            int index = Array.IndexOf(numbers, 9);
+            Console.WriteLine("Index of 9: " + index);
         }
     }
 }
