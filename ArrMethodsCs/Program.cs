3b00a5651e65a63dc197077016accda3bcdf5f17
masen.funderburk@gmail.com <masen.funderburk@gmail.com>
2021-11-05 22:21:54 -0500

    array practice finished

--- ArrMethodsCs/Program.cs
+++ ArrMethodsCs/Program.cs
@@ -13,7 +13,42 @@ namespace ArrMethodsCs
 
             //IndexOf()
             int index = Array.IndexOf(numbers, 9);
+            //searches for index IndexOf(Source arr, value to look for index)
             Console.WriteLine("Index of 9: " + index);
+
+            //Clear()
+            Array.Clear(numbers, 0, 2);
+            //Will set the range of elements to the default value Clear(Source arr, starting index, how many elements affected)
+            foreach(int n in numbers)
+            {
+                Console.WriteLine(n);
+            }
+
+            //Copy()
+            int[] another = new int[3];
+            Array.Copy(numbers, another, 3);
+            //will copy starting elements from source array to new array Copy(Source array, destination array, number of elements copied)
+            Console.WriteLine("Effect of Copy()");
+            foreach (int n in another)
+            {
+                Console.WriteLine(n);
+            }
+
+            //Sort();
+            Array.Sort(numbers);
+            //Sorts array from least to greatest Sort(Source arr)
+            Console.WriteLine("Effect of Sort()");
+            foreach (int n in numbers)
+            {
+                Console.WriteLine(n);
+            }
+
+            //Reverse()
+            Array.Reverse(numbers);
+            foreach (int n in numbers)
+            {
+                Console.WriteLine(n);
+            }
         }
     }
 }
